6418a9b711d1bbea14d2f2dc8ca94bba0bbe66c2
Nathaniel <nathaniel.c.atkinson@gmail.com>
2026-07-07 23:14:37 -0600

    more updates to menu

--- public/global/scripts.js
+++ public/global/scripts.js
@@ -1,10 +1,13 @@
+const header = document.querySelector("header");
+const nav = document.querySelector("nav");
+const body = document.querySelector("body");
 const main = document.querySelector("main");
     const main_L = document.querySelector("main .left");
     const main_M = document.querySelector("main .middle");
     const main_R = document.querySelector("main .right");
 const footer = document.querySelector("footer");
 
-export const globalConsts = {main, main_L, main_M, main_R, footer};
+export const globalConsts = {body, nav, main, main_L, main_M, main_R, footer};
 
 function createFooter() {
     const data = ["INTEL SCREENING", "From: NATHANIEL ATKINSON", "5 JULY 2026"];
--- public/home/scripts.js
+++ public/home/scripts.js
@@ -1,29 +1,44 @@
 import {globalConsts} from "/api/global/scripts";
 
-const {main_M} = globalConsts;
+const {body, main_M, main, nav, header, footer, main_L, main_R} = globalConsts;
 
 const createButton = document.querySelector("#openCreateMenu");
 createButton.addEventListener("click", (e) => {
     let menu = document.querySelector("#createMenu");
+    const button = menu?.querySelector('input');
+
+    const rect = createButton.getBoundingClientRect();
+    const {top, left, right, bottom, width, height, x, y} = rect;
 
     if (!menu) {
-        const menu = document.createElement("div");
-        menu.id="createMenu";
+        menu = document.createElement("div");
+        menu.id = "createMenu";
+        menu.style.position = "fixed";
+        menu.style.left = `${right + 10}px`
+        menu.style.top = `${top}px`
+        menu.style.zIndex = 100;
+        menu.style.backgroundColor = "var(--background)";
+        menu.style.color = "var(--color)";
+        menu.style.border = "3px solid var(--border)";
+        menu.style.width = "fit-content";
+        menu.style.height = "fit-content";
+        menu.style.padding = "10px";
         menu.innerHTML =`
+            <span class="underlined bold">OPTIONS</span>
             <p>Database</p>
             <p>Database Table</p>
             <p>Database Table Record</p>
         `;
         
-        createButton.parentElement.classList.add("active");
+        createButton.classList.add("active");
 
-        main_M.appendChild(menu);
+        body.appendChild(menu);
 
         document.querySelectorAll("#createMenu p").forEach(i => {i.classList.add("button");})
 
     } else {
         menu.remove();
-        createButton.parentElement.classList.remove("active");
+        createButton.classList.remove("active");
     }
     
 })
